d0325632473e4777ec5549734accfeb94b9f107f
moqumo <mokumoku2166@gmail.com>
2024-12-10 15:01:23 +0900

    analysisのhanldler定義

--- server/src/server/__main__.py
+++ server/src/server/__main__.py
@@ -2,11 +2,31 @@ from utils.lib import greet
 import uvicorn
 from fastapi import FastAPI
 
+from models.activity import Activity
+from storage.json_storage import JsonStorage
+
+from analysis.analysis import Analysis, AnalysisInput, AnalysisOutput, BaseOutput
+
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": greet()}
+
+@app.post("/analysis")
+async def analysis(user_id: str):
+    storge = JsonStorage("test_data/test_activities.json")
+    activities = storge.read_user_activities(user_id)
+    input = AnalysisInput(activities=activities)
+    output = Analysis(input).output()
+    return {
+        "per_total": output.per_total.dict(),
+        "per_year": output.per_year.dict(),
+        "per_month": output.per_month.dict(),
+        "per_week": output.per_week.dict(),
+        "per_day": output.per_day.dict()
+    }
 
 if __name__ == "__main__":
     uvicorn.run(app)
