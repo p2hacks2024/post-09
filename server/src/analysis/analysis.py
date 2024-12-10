81e5628036c39697a2ea77cdf3cb620d19d0dc41
moqumo <mokumoku2166@gmail.com>
2024-12-10 10:29:14 +0900

    analysisの核部分

--- server/src/analysis/analysis.py
+++ server/src/analysis/analysis.py
@@ -1,19 +1,35 @@
-
+from pydantic import BaseModel
 from typing import List
 
 from models.activity import Activity
 from storage.json_storage import JsonStorage
 
+'''
+exapmle:
+    input = AnalysisInput(activities)
+    output = Analysis(input).output()
+'''
+
+class AnalysisInput(BaseModel):
+    '''
+    分析用のリクエストボディ
+    '''
+    activities: List[Activity]
+
+class AnalysisOutput(BaseModel):
+    '''
+    分析結果のレスポンスボディ
+    '''
+    emotion_histgram: dict # 感情ごとの出現回数
+
 class Analysis:
-    def __init__(self, storage_path: str):
-        '''
-        storage_path: str - アクティビティの保存先
-        storage: JsonStorage - JsonStorageのインスタンス
-        activities: List[Activity] - アクティビティのリスト
-        '''
-        self.storage_path = storage_path
-        self.storage = JsonStorage(self.storage_path)
-        self.activities = self.storage.load_activities()
+    def __init__(self, activities: List[Activity]):
+        self.activities = activities
+
+    def output(self) -> AnalysisOutput:
+        output = AnalysisOutput()
+        output.emotion_histgram = self.get_enotion_histgram()
+        return output
 
     def get_enotion_histgram(self) -> dict:
         pass
