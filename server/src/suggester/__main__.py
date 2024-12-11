a885aed913375b44182742da7a700ce50b2c6dfe
jigsaw-glitch <188816911+jigsaw-glitch@users.noreply.github.com>
2024-12-11 14:41:56 +0900

    A temporary provisional commit of Suggester

--- server/src/suggester/__main__.py
+++ server/src/suggester/__main__.py
@@ -1,33 +1,36 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-from icecream import ic
-import json
-import os
-
-# spotifyインスタンスを作成
-sp = spotipy.Spotify(
-    auth_manager=SpotifyClientCredentials(
-        client_id="f2b07462203a4302866b30693c730e0e",  # Client ID
-        client_secret="df34cab6e70d4a17b2c2b2a724143a39",  # Client Secret
-    )
-)
-
-track_ids = []
-results = sp.search(q="LE SSERAFIM", limit=5, type="track")
-# ic(results["tracks"]["items"])
-for track in results["tracks"]["items"]:
-    track_id = track["id"]
-    track_ids.append(track_id)
+from pydantic import BaseModel
+from suggester import llm
 
 
-# スクリプトがあるディレクトリを取得
-script_dir = os.path.dirname(os.path.realpath(__file__))
+# I/O
+class SuggesterInput(BaseModel):
+    """
+    Suggesterの入力情報
+    """
 
-# 保存先ファイルパスを設定
-file_path = os.path.join(script_dir, "track_ids.json")
+    emotion: str
+    prompt: str
+    # activities: List[Activity]
 
-# JSONに保存
-with open(file_path, "w") as json_file:
-    json.dump(track_ids, json_file, indent=4)
 
-print("Track IDs saved to track_ids.json")
+class SuggesterOutput(BaseModel):
+    """
+    Suggesterの出力情報
+    """
+
+    # musics: List[Music]
+    situation: str
+    comment: str
+
+
+class Suggester(BaseModel):
+    input: SuggesterInput
+
+    def llm_runner(self) -> SuggesterOutput:
+        model_path = llm.get_model_path()
+        emotion_analyzer = llm.EmotionAnalyzer(model_path=model_path)
+        prompt = SuggesterInput.prompt
+        result = emotion_analyzer.analyze_emotion(prompt)
+        output = SuggesterOutput(situation=result, comment=result)
+
+        return output
