9605c1fb31dc1aae1057cd5a710252bb67a5ed27
Teruki Tada <me@peruki.dev>
2024-12-11 14:39:04 +0900

    cors

--- server/src/server/__main__.py
+++ server/src/server/__main__.py
@@ -1,12 +1,28 @@
+import os
+from dotenv import load_dotenv
 from utils.lib import greet
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+load_dotenv(verbose=True)
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(os.environ.get("FRONTEND_ORIGIN"))],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
     return {"message": greet()}
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
