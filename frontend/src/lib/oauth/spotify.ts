cf78e314f91a263a14c898bc968766876092e33c
Teruki Tada <me@peruki.dev>
2024-12-11 13:44:20 +0900

    accessToken -> access_token

--- frontend/src/lib/oauth/spotify.ts
+++ frontend/src/lib/oauth/spotify.ts
@@ -72,16 +72,16 @@ export class AuthController {
 	handleAuthCallback(scene: string) {
 		const fragmentString = window.location.hash.substring(1);
 		const params = new URLSearchParams(fragmentString);
-		const accessToken = params.get('accessToken');
+		const accessToken = params.get('access_token');
 		if (accessToken) {
-			localStorage.setItem(this.item_oauth_params, JSON.stringify({ accessToken: accessToken }));
+			localStorage.setItem(this.item_oauth_params, JSON.stringify({ access_token: accessToken }));
 			window.history.replaceState({}, document.title, window.location.origin + '?scene=' + scene);
 		}
 	}
 
 	accessToken(): string {
 		const params = JSON.parse(localStorage.getItem(this.item_oauth_params) || '{}');
-		return params['accessToken'] || '';
+		return params['access_token'] || '';
 	}
 
 	async checkAuthorized(): Promise<AuthInfo> {
@@ -89,7 +89,7 @@ export class AuthController {
 		if (accessToken) {
 			let response = new Promise((resolve, _reject) => {
 				const xhr = new XMLHttpRequest();
-				xhr.open('GET', 'https://api.spotify.com/v1/me?' + 'accessToken=' + accessToken);
+				xhr.open('GET', 'https://api.spotify.com/v1/me?' + 'access_token=' + accessToken);
 				let signinFunc = this.oauth2SignIn;
 				xhr.onreadystatechange = function (_) {
 					if (xhr.readyState === 4 && xhr.status === 200) {
--- frontend/src/scenes/Analysis.svelte
+++ frontend/src/scenes/Analysis.svelte
@@ -1,9 +1,29 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import AccountConfig from '../components/AccountConfig.svelte';
 	import type { AuthInfo, AuthController } from '../lib/oauth/spotify';
 
 	export let authInfo: AuthInfo | null;
 	export let authController: AuthController;
+
+	let analysis: any;
+
+	onMount(async () => {
+		if (!authInfo?.signedIn()) {
+			return;
+		}
+		analysis = await fetch(
+			`http://${import.meta.env.VITE_API_SERVER_URL}/analysis/${authInfo?.getUserId()}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		).then((res) => res.json());
+
+		console.log(analysis);
+	});
 </script>
 
 <div class="h-full text-fwhite">
