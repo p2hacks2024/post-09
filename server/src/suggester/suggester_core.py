d4c360ede3894edcdf6433b5eefc7770be740b25
jigsaw-switch <188816911+jigsaw-glitch@users.noreply.github.com>
2024-12-12 21:34:59 +0900

    update suggester_core

--- server/src/suggester/suggester_core.py
+++ server/src/suggester/suggester_core.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from suggester.suggester_models import SuggesterInput, SuggesterOutput
 from models.activity import Activity, Music
 from typing import List
+from suggester import llm
 
 
 class Suggester(BaseModel):
@@ -12,8 +13,10 @@ class Suggester(BaseModel):
         model_path = llm.get_model_path()
         emotion_analyzer = llm.EmotionAnalyzer(model_path=model_path)
         prompt = SuggesterInput.prompt
-        result = emotion_analyzer.analyze_emotion(prompt)
-        output = SuggesterOutput(situation=result, comment=result)
+        response = emotion_analyzer.analyze_emotion(prompt)
+        musics = emotion_analyzer.get_ids(response.music_query)
+        # output = SuggesterOutput(situation=result, comment=result)
+        output = emotion_analyzer.get_suggester_output(musics, response)
 
         # dummuy output
         # musics = [
