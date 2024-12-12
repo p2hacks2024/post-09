37a954e894f5040f49b65cb58af0c86ecb4a215e
moqumo <mokumoku2166@gmail.com>
2024-12-12 13:04:03 +0900

    修正＆テスト済み

--- server/src/storage/json_storage.py
+++ server/src/storage/json_storage.py
@@ -1,61 +1,67 @@
 import json
-from typing import List
+from typing import List, Any
 
-from models.activity import Activity
-from storage.storage import Storage
+from models.activity import Activity  #  pyright: ignore
+from storage.storage import Storage  # pyright: ignore
+
 
 class JsonStorage(Storage):
-    '''
+    """
     JSONファイルを直接読み書きするためのクラス
-    '''
+    """
+
     def __init__(self, file_path: str):
         self.file_path = file_path
 
-    def _read_file(self) -> dict:
-        with open(self.file_path, 'r') as f:
+    def _read_file(self) -> dict[str, Any]:
+        with open(self.file_path, "r") as f:
             return json.load(f)
 
-    def _write_file(self, data: dict):
-        with open(self.file_path, 'w') as f:
+    def _write_file(self, data: dict[str, Any]):
+        with open(self.file_path, "w") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
-    
+
     def create_user_activities(self, user_id: str, activities: List[Activity]):
-        user_data_dict = {'user_id': user_id, 'activities': [activity.model_dump() for activity in activities]}
+        user_data_dict: dict[str, Any] = {
+            "user_id": user_id,
+            "activities": [activity.model_dump() for activity in activities],
+        }
         data_dict = self._read_file()
-        for user in data_dict['users']:
-            if user['user_id'] == user_id:
-                raise ValueError(f'User {user_id} already exists.')
-        data_dict['users'].append(user_data_dict)
+        for user in data_dict["users"]:
+            if user["user_id"] == user_id:
+                raise ValueError(f"User {user_id} already exists.")
+        data_dict["users"].append(user_data_dict)
         self._write_file(data_dict)
         pass
 
     def read_user_activities(self, user_id: str) -> List[Activity]:
         data_dict = self._read_file()
-        for user in data_dict['users']:
-            if user['user_id'] == user_id:
-                return [Activity(**activity) for activity in user['activities']]
-        return [] #もしuser_idが存在しない場合は空リストを返す
+        for user in data_dict["users"]:
+            if user["user_id"] == user_id:
+                return [Activity(**activity) for activity in user["activities"]]
+        return []  # もしuser_idが存在しない場合は空リストを返す
 
     def update_user_activities(self, user_id: str, activities: List[Activity]):
         data_dict = self._read_file()
-        if not data_dict['users']:
+        if not data_dict["users"]:
             # ユーザーデータが空の場合は新規作成
             self.create_user_activities(user_id, activities)
             return
         else:
             # ユーザーデータが存在する場合は更新
-            for user in data_dict['users']:
-                if user['user_id'] == user_id:
-                    user['activities'] = [activity.model_dump() for activity in activities]
+            for user in data_dict["users"]:
+                if user["user_id"] == user_id:
+                    user["activities"] = [
+                        activity.model_dump() for activity in activities
+                    ]
                     break
         self._write_file(data_dict)
-    
+
     def delete_user_activities(self, user_id: str):
         data_dict = self._read_file()
-        for user in data_dict['users']:
-            if user['user_id'] == user_id:
-                data_dict['users'].remove(user)
+        for user in data_dict["users"]:
+            if user["user_id"] == user_id:
+                data_dict["users"].remove(user)
                 break
         self._write_file(data_dict)
         pass
-    
