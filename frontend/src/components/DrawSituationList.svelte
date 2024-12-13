b986bd9a479e674e007565a8c7035033d6705898
moqumo <mokumoku2166@gmail.com>
2024-12-13 15:54:33 +0900

    出来事リスト表示のコンポーネント

---
+++ frontend/src/components/DrawSituationList.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+    export let createSituationList: () => string[];
+    export let chosenEmotion: string;
+	let outputText: string[] = [];
+
+    onMount(() => {
+		outputText = ["ここに状況が表示されます"];
+	});
+
+    $: if(chosenEmotion){
+        outputText = createSituationList();
+    }
+</script>
+
+{#each outputText as text}
+        <p class="text-lg">{text}</p>
+{/each}
