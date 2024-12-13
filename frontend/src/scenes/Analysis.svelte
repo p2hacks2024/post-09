2de4e684996ad2c507441fa1a1a299184b5f52c2
moqumo <mokumoku2166@gmail.com>
2024-12-13 13:53:58 +0900

    既にあったemotionTableを削除

--- frontend/src/scenes/Analysis.svelte
+++ frontend/src/scenes/Analysis.svelte
@@ -53,16 +53,6 @@
         // ヒストグラムデータ
 		const emotionFreq: { [key: string]: number } = analysis["per_total"]["emotion_freq"];
 		console.log(emotionFreq["sad"] || 0)
-        const emotionTable = {
-            "envious": "嫉妬",
-            "disgusting": "嫌悪",
-            "asshamed": "恥",
-            "lonely": "孤独感",
-            "angry": "怒り",
-            "anxious": "不安",
-            "fear": "恐怖",
-            "complicated": "複雑"
-        };
 
 		console.log(root.clientWidth, root.clientHeight)
         // ヒストグラムのバーを作成
